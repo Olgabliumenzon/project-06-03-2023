cd9fe18930d8542def56ddd15fc457f41ea01afc
a839562 <olgblium@gmail.com>
2023-03-06 20:58:27 +0200

    calculator file created

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    // psvm - a magic shortcut for public static void main
+
+    public static void main(String[] args) {
+        System.out.println("Calculator ...");
+    }
+}
