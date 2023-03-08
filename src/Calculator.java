1eec6959c80ae18e85dcf40e31daa1fa8923567e
a839562 <olgblium@gmail.com>
2023-03-08 18:58:07 +0200

    with excercise 8

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,5 +18,45 @@ public class Calculator {
         System.out.println("You provided number: " + secondNumber);
         int sum = firstNumber + secondNumber;
         System.out.println("Sum is: " + sum);
+
+        System.out.println("Calculating difference of two numbers");
+        System.out.println("Provide third number: ");
+        int thirdNumber;
+        int fourthNumber;
+        Scanner inputReader = new Scanner(System.in);
+        thirdNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + thirdNumber);
+        System.out.println("Provide fourth number");
+        fourthNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + fourthNumber);
+        int difference = thirdNumber - fourthNumber;
+        System.out.println("Difference is: " + difference);
+
+
+        System.out.println("Calculating product of two numbers");
+        System.out.println("Provide fifth number: ");
+        int fifthNumber;
+        int sixthNumber;
+        Scanner inputReader = new Scanner(System.in);
+        fifthNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + fifthNumber);
+        System.out.println("Provide sixth number");
+        sixthNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + sixthNumber);
+        int product = fifthNumber * sixthNumber;
+        System.out.println("Product is: " + product);
+
+        System.out.println("Calculating quotient of two numbers");
+        System.out.println("Provide fifth number: ");
+        int seventhNumber;
+        int eightNumber;
+        Scanner inputReader = new Scanner(System.in);
+        seventhNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + seventhNumber);
+        System.out.println("Provide seventh number");
+        eightNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + eightNumber);
+        int quotient = fifthNumber / sixthNumber;
+        System.out.println("Product is: " + quotient);
     }
 }
