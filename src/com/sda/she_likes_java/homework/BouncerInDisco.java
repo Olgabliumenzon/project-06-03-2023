19d078e7635dcacbaf1b6ef2d69275f1d4898b85
a839562 <olgblium@gmail.com>
2023-03-10 18:39:22 +0200

    Bouncer disco start of excercise with correct code

--- src/com/sda/she_likes_java/homework/BouncerInDisco.java
+++ src/com/sda/she_likes_java/homework/BouncerInDisco.java
@@ -1,12 +1,29 @@
 package com.sda.she_likes_java.homework;
 
+import java.util.Scanner;
+
 public class BouncerInDisco
 {
     public static void main(String[] args) {
         System.out.println("Bouncer disco");
 
-        int age = 15;
-        boolean isKid = age < 18;
-        System.out.println("is kid: " + isKid);
+
+
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Tell me how old are you and what is your height");
+        int personAge = inputReader.nextInt();
+        int personHeight = inputReader.nextInt();
+
+        if (personAge >= 16 && personHeight >= 164) {
+            System.out.println("You are over 16 and so tall! Great! Welcome to the disco! Enjoy it!");
+        } else {
+            System.out.println("Sorry, not this time, kid :( ");
+        }
     }
 }
+
+
+
+
+
+
