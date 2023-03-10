5b7485d281a83270c5886aea02980b39d75c71be
a839562 <olgblium@gmail.com>
2023-03-10 17:12:42 +0200

    Bouncer disco start of excercise

---
+++ src/com/sda/she_likes_java/homework/BouncerInDisco.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.homework;
+
+public class BouncerInDisco
+{
+    public static void main(String[] args) {
+        System.out.println("Bouncer disco");
+
+        int age = 15;
+        boolean isKid = age < 18;
+        System.out.println("is kid: " + isKid);
+    }
+}
