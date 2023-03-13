a55b40cc9101b4b24fd59be4fff85a29effc0cbe
a839562 <olgblium@gmail.com>
2023-03-13 20:14:16 +0200

    while loop second example

---
+++ src/com/sda/she_likes_java/loops/while_loop/WhileIGrow.java
@@ -0,0 +1,17 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class WhileIGrow {
+
+    public static void main(String[] args) {
+        int ageOfTheKid = 1;
+
+        while (ageOfTheKid <= 18) {
+            System.out.println("Mommy can I go to disco??");
+            System.out.println("No");
+
+            ageOfTheKid++; // this is shorter version of:  ageOfTheKid = ageOfTheKid + 1;
+        }
+
+        System.out.println("Go to party!!!");
+    }
+}
