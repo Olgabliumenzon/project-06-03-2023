d5489468912ab82ebc20d19edd98a54ceb9704a5
a839562 <olgblium@gmail.com>
2023-03-27 20:26:54 +0300

    constructor example on 27.03.2023

---
+++ src/com/sda/she_likes_java/classes/Address.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.classes.constructor;
+
+public class FullAddress {
+    public String country;
+    public String city;
+    public String street;
+    // 02-987
+    // 011
+    public String postalCode;
+}
