cec9e96f58c1c3af4f5e4b88173b5b49ec9d139e
a839562 <olgblium@gmail.com>
2023-03-27 22:54:59 +0300

    Class Full Address deleted

---
+++ src/com/sda/she_likes_java/homework/exercise15/HomeworkExercise15.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.homework.exercise15;
+
+public class HomeworkExercise15 {
+    /*Exercise 15.
+Create a Parent class containing the following variables:
+- first name
+- surname
+- address - (use the class Address)
+- firstChild (use class Person)
+- secondChild (use class Person)
+
+     */
+
+}
--- src/com/sda/she_likes_java/homework/exercise15/Parent.java
+++ src/com/sda/she_likes_java/homework/exercise15/Parent.java
@@ -1,4 +1,22 @@
 package com.sda.she_likes_java.homework.exercise15;
 
+/*    /*Exercise 15.
+Create a Parent class containing the following variables:
+- first name
+- surname
+- address - (use the class Address)
+- firstChild (use class Person)
+- secondChild (use class Person)
+*/
 public class Parent {
+    String firstName;
+    String surname;
+    Address address;
+    Person firstChild;
+    Person secondChild;
+
 }
+    public static void main(String[] args) {
+        System.out.println();
+        Address
+    }
