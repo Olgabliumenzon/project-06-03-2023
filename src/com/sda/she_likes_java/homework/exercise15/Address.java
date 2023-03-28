487e6bebe7803341c8459f0d16e830427eeace4f
a839562 <olgblium@gmail.com>
2023-03-28 22:49:20 +0300

    public class address added

--- src/com/sda/she_likes_java/homework/exercise15/Address.java
+++ src/com/sda/she_likes_java/homework/exercise15/Address.java
@@ -1,4 +1,12 @@
 package com.sda.she_likes_java.homework.exercise15;
 
 public class Address {
+    String country;
+    String city;
+    String street;
+    String postalCode;
+
+    // it can be String postalCode if postal code holds letters or other symbols
+
+
 }
