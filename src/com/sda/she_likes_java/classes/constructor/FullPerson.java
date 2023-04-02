ac6e0011f42559a14599e664780271a740a5c1cb
a839562 <olgblium@gmail.com>
2023-04-03 00:34:57 +0300

    house example

--- src/com/sda/she_likes_java/classes/constructor/FullPerson.java
+++ src/com/sda/she_likes_java/classes/constructor/FullPerson.java
@@ -1,11 +1,11 @@
 package com.sda.she_likes_java.classes.constructor;
 
 public class FullPerson {
-    String name;
-    String surname;
-    int age;
+    private String name;
+    private String surname;
+    private int age;
 
-    FullAddress address;
+    private FullAddress address;
 
     public FullPerson() {
     }
@@ -14,6 +14,40 @@ public class FullPerson {
         this.name = name;
         this.surname = surname;
         this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age >0 && age < 150) {
+            this.age = age;
+        }
+    }
+
+    public FullAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(FullAddress address) {
         this.address = address;
     }
 
@@ -29,11 +63,7 @@ public class FullPerson {
 
     public void describeYourself() {
         System.out.println("Hi I'm " + name);
-
-        public static void genericDescription() {
-            System.out.println("Hi I'm " + name);
-        }
-
+    }
 
     public static void main(String[] args) {
         FullAddress address = new FullAddress("Italy", "Roma", "La Romania", "007");
@@ -44,7 +74,5 @@ public class FullPerson {
 
         FullPerson personWithoutAddress = new FullPerson("Maria", "Kowalska", 18, null);
         personWithoutAddress.describeYourself();
-
-        genericDescription();
     }
 }
