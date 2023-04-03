10945e43ea3797afb18f8d6b8ea1c16c3d1ba03d
a839562 <olgblium@gmail.com>
2023-04-03 21:39:19 +0300

    washmachine with various static examples

--- src/com/sda/she_likes_java/static_modifier/WashMachine.java
+++ src/com/sda/she_likes_java/static_modifier/WashMachine.java
@@ -1,51 +1,41 @@
 package com.sda.she_likes_java.static_modifier;
-
 public class WashMachine {
-
     private static int numberOfCreatedMachines;
     private String brand;
     private String model;
     private int serialNumber;
-
     public WashMachine(String brand, String model) {
         this.brand = brand;
         this.model = model;
         numberOfCreatedMachines++;
         serialNumber++;
     }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-
     public static void setNumberOfCreatedMachines(int numberOfCreatedMachines) {
         WashMachine.numberOfCreatedMachines = numberOfCreatedMachines;
     }
-
-    public int getSerialNumber() {
-        return serialNumber;
+    // instance method
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
     }
-
+    // class method
     public static int getNumberOfCreatedMachines() {
         return numberOfCreatedMachines;
     }
-
+    // instance method
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+    // instance method
     public String getBrand() {
         return brand;
     }
-
     public String getModel() {
         return model;
     }
-
+    // instance method
+    public void increaseSerialNumber() {
+        serialNumber++;
+    }
     @Override
     public String toString() {
         return "WashMachine{" +
