112adc2308c02d25146b0fc3a84e45743523e316
a839562 <olgblium@gmail.com>
2023-04-27 22:40:32 +0300

    exercise 20

---
+++ src/com/sda/she_likes_java/homework/exercise_20/Exercise20.java
@@ -0,0 +1,15 @@
+package com.sda.she_likes_java.homework.exercise_20;
+//Exercise 20
+//Create a Car class with a drive() method.
+//Create 3 subclasses:
+//- Mazda
+//- Toyota
+//- Ferrari
+//Override the drive() method in each of the subclasses.
+//In addition, create three unique methods in each subclass.
+//Create a class CarUtils with method safeCast(Car car)
+//- inside it, try a safe cast for Mazda and Toyota to call specific methods.
+//- if the object sent is of a different type - just output the information to the console.
+//- have fun
+public class Exercise20 {
+}
