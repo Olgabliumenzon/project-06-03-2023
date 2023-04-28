c4951303b7154a44a8f87e383a1bfb603016d542
a839562 <olgblium@gmail.com>
2023-04-28 08:42:29 +0300

    Moveable interface introduced

--- src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
+++ src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Bird bird = new Bird();
-       // bird.fly();
+//        bird.fly();
 
         Animal tiger = new Tiger();
         tiger.go();
@@ -16,6 +16,22 @@ public class Main {
         Flyable flyableParrot = new Parrot();
 
         Swimmable tiger2 = (Swimmable) tiger;
+        Swimmable swimmableSuperman = (Swimmable) superman;
         Swimmable swimmableTiger = new Tiger();
+
+        Superman superSuperMan = new Superman();
+        Flyable superFly = superSuperMan;
+        Swimmable superSwim = superSuperMan;
+
+        // Tiger assignment ways
+        Tiger newTiger = new Tiger();
+        newTiger.go();
+        newTiger.swim();
+        Animal animalTiger = new Tiger();
+        animalTiger.go();
+        Swimmable swimmableTiger2 = new Tiger();
+        swimmableTiger2.swim();
+        Object objectTiger = new Tiger();
+        // cannot call go() and swim() - they are not visible here
     }
 }
