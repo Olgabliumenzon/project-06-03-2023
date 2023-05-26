74855deeb9db5ccd432175d6eb36df66eb3150c5
a839562 <olgblium@gmail.com>
2023-05-26 21:33:58 +0300

    Application for getting data from Test - almost working

---
+++ src/com/sda/she_likes_java/database/DatabaseUtils.java
@@ -0,0 +1,50 @@
+package com.sda.she_likes_java.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtils {
+
+    /**
+     * Script for creating table
+     * CREATE TABLE TEST
+     * (ID INT PRIMARY KEY, NAME VARCHAR(255));
+     *
+     * Script for inserting one record (row) into table
+     * INSERT INTO
+     * TEST(ID, NAME)
+     * VALUES(1, 'Olga');
+     *
+     * Script for reading data
+     * SELECT ID, NAME
+     * FROM TEST;
+     *
+     * Script for updating data
+     * UPDATE TEST -- name of table to update
+     * SET NAME = 'Alma' -- column value to change
+     * WHERE ID=2; -- selected row
+     *
+     * Script for deleting data
+     * DELETE
+     * FROM TEST
+     * WHERE ID=3;
+     */
+    // handle internally
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech\\db",
+                    "sa",
+                    "sa");
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    // declare
+    public static Connection getConnectionButSometimesThrowException() throws SQLException {
+        return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech\\db",
+                "sa",
+                "sa");
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/exercise_22/Main.java
@@ -0,0 +1,43 @@
+package com.sda.she_likes_java.homework.exercise_22;
+/package com.sda.she_likes_java.homework.exercise_22;
+
+/**
+ * Create a shopping cart class.
+ * Add as a field an items variable of type List<Item> - to store products.
+ * Item is also the class to be created.
+ * It is to contain the following fields:
+ * - product name
+ * - product price.
+ * The shopping cart class should provide the following operations:
+ * - add product to basket
+ * - displaying products in the shopping cart
+ * - calculation of the total basket value.
+ * Create a Main class and check the operations of your classes.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Let's buy something for barbecue");
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Item beer = new Item("beer", 10);
+        Item sausages = new Item("sausages", 5.5);
+        Item meat = new Item("meat", 17.5);
+
+        shoppingCart.addProductToBasket(beer);
+        shoppingCart.addProductToBasket(beer);
+        shoppingCart.addProductToBasket(beer);
+        shoppingCart.addProductToBasket(beer);
+        shoppingCart.addProductToBasket(beer);
+        shoppingCart.addProductToBasket(sausages);
+        shoppingCart.addProductToBasket(meat);
+
+        System.out.println("Show me what's in the shopping cart");
+        shoppingCart.displayProducts();
+
+        System.out.println("The total price of the basket of products is: " + shoppingCart.calculateTotalBasketValue());
+    }
+}
+
+/
