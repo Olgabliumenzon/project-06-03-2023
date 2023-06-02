7fedfed697ad6931f9d49c1fed9e2a802b3f826c
a839562 <olgblium@gmail.com>
2023-06-02 21:15:11 +0300

    list examples

--- src/com/sda/she_likes_java/homework/exercise_22/ShoppingCart.java
+++ src/com/sda/she_likes_java/homework/exercise_22/ShoppingCart.java
@@ -21,9 +21,12 @@ public class ShoppingCart {
 
     public double calculateTotalBasketValue() {
         double sum = 0;
-        for (int i = 0; i < items.size(); i++) { // items.size() - is like array.length, or number of elements inside the list
-            sum += items.get(i).getPriceInUSD(); //same as sum = sum + items.get(i).getPriceInUSD();
+        for (Item item : items) {
+            sum += item.getPriceInUSD();
         }
+//        for (int i = 0; i < items.size(); i++) {
+//            sum += items.get(i).getPriceInUSD();
+//        }
 
         return sum;
     }
