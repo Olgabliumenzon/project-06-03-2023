86919eb18c8769ec8f46c32eb74d95a1b364a448
a839562 <olgblium@gmail.com>
2023-06-13 10:43:41 +0300

    Exceptions introduction

---
+++ src/com/sda/she_likes_java/exceptions/BankConnector.java
@@ -0,0 +1,22 @@
+package com.sda.she_likes_java.exceptions;
+
+import java.util.Random;
+
+public class BankConnector {
+
+    public Integer accountBalance() throws Exception {
+        // trying to connect to real account :)
+        Random random = new Random();
+        int randomNumber = random.nextInt();
+
+        // 100 % 2 -> 0
+        // 101 % 2 -> 1
+        if (randomNumber % 2 == 0) {
+            return 1000;
+        } else {
+            System.out.println("Network error");
+            throw new Exception("Connection error");
+            // exception is like 404 error message in browser
+        }
+    }
+}
